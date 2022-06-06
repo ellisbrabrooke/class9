4acc0065077259270ef3bc8e2d47c008b92a7999
ellisbrabrooke <106995864+ellisbrabrooke@users.noreply.github.com>
2022-06-06 19:42:29 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box =  createSprite(200,200,20,20)
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites();
+  if(keyIsDown(01100100)){
+    box.x = box.x+5;
+  }
+}
+
+
+
+
